419aedb7088ccd33b1c48354f5c18140e4043e84
LeandroCanuto <leandrocanutto@hotmail.com>
2022-09-20 20:49:04 -0300

    inseridas politicas

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 
+ window.onload= slide1
 
+function slide1(){
+    document.getElementById('img').src="img/iphone4.png"
+    setTimeout("slide2()", 2000)
+    }
+    
+    function slide2(){
+    document.getElementById('img').src="img/iphone2.png"
+    setTimeout("slide3()", 2000)
+    }
+    
+    function slide3(){
+    document.getElementById('img').src="img/iphone3.png"
+    setTimeout("slide1()", 2000)
+    }
